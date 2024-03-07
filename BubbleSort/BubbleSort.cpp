fc439ebbcdc01f4953b59e1e8e3fe6c3223e122a
syairaarif <putrisyafira4444@gmail.com>
2024-03-07 12:15:14 +0700

    menampilkan hasil/Output

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -7,7 +7,44 @@ using namespace std;
 int a[20];   // deklarasi array a dengan ukuran 20
 int n;       // deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
+void input()
+{       // procedure untuk input
+	while (true)
+	{   // looping
 
-int main() {
-	return 0;
+		cout << "masukan banyaknya elemen pada array : ";   // output ke layar
+		cin >> n;        // input dari pengguna
+
+		if (n <= 20)
+		{// jika n kurang dari atau sama dengan 20
+			break;
+		}// keluar dari loop
+		else
+		{          // jika n lebih dari 20
+			cout << "n\array dapat mempunyai maksimal 20 elemen.\n" << endl;  //output ke layar
+		}
+	}
+
+	cout << endl;                                // output baris kpsong
+	cout << "===================" << endl;       //output ke layar
+	cout << "masukan elemen array : " << endl;   // output ke layar
+
+	for (int i = 0; i < n; i++) {                  //looping dari i dimulai dari 0 hingga n-1
+		cout << "data ke- " << (i + 1) << ":";   // output ke layar
+		cin >> a[i];
+	}
 }
+
+void display() {                                  //procedure untuk menampilkan hasil
+	cout << endl;                                 //output baris kosong
+	cout << "===================" << endl;        // output ke layar
+	cout << "elemen array yang telah tersusun" << endl;  //output ke layar
+	cout << "===================" << endl;        //output ke layar
+	for (int j = 0; j < n; j++) {                 //looping dengan j dimulai dengan 0 hingga n-1
+		cout << "data ke- " << j + 1 << ":";
+		cout << a[j] << endl;                     //output ke layar
+	}
+	cout << endl;
+
+}
+
